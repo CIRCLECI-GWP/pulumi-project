31a318e37a3922af379c596f53e2d6601f5475bb
yemiwebby <yemiwebby@gmail.com>
2025-05-21 23:17:13 +0100

    update config

--- __main__.py
+++ __main__.py
@@ -45,19 +45,23 @@ cluster = container.Cluster(
     initial_node_count=2,
     min_master_version='latest',
     node_version='latest',
-    node_config={
-        'machine_type': machine_type,
-        'disk_type': 'pd-standard',
-        'disk_size_gb': 100,
-        'oauth_scopes': [
+    enable_private_nodes=True, 
+    node_config=container.ClusterNodeConfigArgs(
+        machine_type=machine_type,
+        disk_type='pd-standard',
+        disk_size_gb=100,
+        oauth_scopes=[
             "https://www.googleapis.com/auth/compute",
             "https://www.googleapis.com/auth/devstorage.read_only",
             "https://www.googleapis.com/auth/logging.write",
             "https://www.googleapis.com/auth/monitoring",
         ],
-        'assign_public_ip': False,
-    }
+        network_interfaces=[container.ClusterNodeConfigNetworkInterfaceArgs(
+            no_external_ip=True
+        )]
+    )
 )
+
 
 # Set the Kubeconfig file values here
 def generate_k8_config(master_auth, endpoint, context):
